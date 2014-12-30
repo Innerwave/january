f14a3e29dea6008ce46dae0501a4648190df4a4d
Hyeong-jin <gilseonappa@gmail.com>
2014-12-30 11:21:41 +0900

    work history : v0.2.393-SNAPSHOT

--- dist/js/innerwave.sheet.js
+++ dist/js/innerwave.sheet.js
@@ -1,7 +1,7 @@
-/*! Innerwave Spreadsheet - v0.2.392-SNAPSHOT - 2014-12-30
+/*! Innerwave Spreadsheet - v0.2.393-SNAPSHOT - 2014-12-30
 * Copyright (c) 2014 innerwave.co.kr; Licensed  */
 /*!
- * Innerwave's jQuery Spreadsheet plugin v0.2.392-SNAPSHOT
+ * Innerwave's jQuery Spreadsheet plugin v0.2.393-SNAPSHOT
  * http://www.innerwave.co.kr
  *
  * Copyright 2014 Innerwave Co., Ltd.
@@ -622,7 +622,7 @@
   */
 
   return $.widget('innerwave.spreadsheet', {
-    version: '0.2.392-SNAPSHOT',
+    version: '0.2.393-SNAPSHOT',
     options: {
       // buttons: [],
       // spreadsheetClass: '',
