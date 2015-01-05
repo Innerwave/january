8907cbe3621e056c94561b88b000481325bd9465
Hyeong-jin <gilseonappa@gmail.com>
2015-01-05 15:21:37 +0900

    work history : v0.2.414-SNAPSHOT

--- src/js/scroller.js
+++ src/js/scroller.js
@@ -1,5 +1,5 @@
 /* 
- * Innerwave Spreadsheet - v0.2.413-SNAPSHOT - 2014-12-30
+ * Innerwave Spreadsheet - v0.2.414-SNAPSHOT - 2014-12-30
  * Copyright (c) 2014 innerwave.co.kr; Licensed
  */
 
@@ -19,7 +19,7 @@
   }
 
   return $.widget('innerwave.scroller', $.ui.slider, {
-    version: '0.2.413-SNAPSHOT',
+    version: '0.2.414-SNAPSHOT',
     widgetEventPrefix: 'iwscroll',
 
     options: {
