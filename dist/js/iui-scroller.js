55540c700d1e9ea97d4c6ea79d973b53fcce4d40
Hyeongjin Yong <gilseonappa@gmail.com>
2015-02-11 16:13:01 +0900

    work history : v0.2.504-SNAPSHOT

--- dist/js/iui-scroller.js
+++ dist/js/iui-scroller.js
@@ -1,4 +1,4 @@
-/*! Innerwave Spreadsheet - v0.2.503-SNAPSHOT - 2015-02-11
+/*! Innerwave Spreadsheet - v0.2.504-SNAPSHOT - 2015-02-11
 * Copyright (c) 2015 innerwave.co.kr; Licensed  */
 ( function ( factory ) {
   if ( typeof define === 'function' && define.amd ) {
@@ -16,7 +16,7 @@
   }
 
   return $.widget( 'iui.scroller', $.ui.slider, {
-    version: '0.2.503-SNAPSHOT',
+    version: '0.2.504-SNAPSHOT',
     widgetEventPrefix: 'iwscroll',
 
     options: {
