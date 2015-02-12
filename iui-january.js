16625573ad5c49906fddf2fc59d956837c2f3d54
Hyeongjin Yong <gilseonappa@gmail.com>
2015-02-12 14:14:27 +0900

    work history : v0.2.567-SNAPSHOT

--- iui-january.js
+++ iui-january.js
@@ -1,4 +1,4 @@
-/*! Innerwave Spreadsheet - v0.2.565-SNAPSHOT - 2015-02-12
+/*! Innerwave Spreadsheet - v0.2.566-SNAPSHOT - 2015-02-12
 * Copyright (c) 2015 innerwave.co.kr; Licensed  */
 ( function ( $, window, undefined ) {
   $.extend( true, window, {
