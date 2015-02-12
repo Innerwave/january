cb8980e56c85d2344cbd8844b1214982eb913077
Hyeongjin Yong <gilseonappa@gmail.com>
2015-02-12 16:08:18 +0900

    work history : v0.2.599-SNAPSHOT

--- iui-january.js
+++ iui-january.js
@@ -1,4 +1,4 @@
-/*! Innerwave Spreadsheet - v0.2.597-SNAPSHOT - 2015-02-12
+/*! Innerwave Spreadsheet - v0.2.598-SNAPSHOT - 2015-02-12
 * Copyright (c) 2015 innerwave.co.kr; Licensed  */
 ( function ( $, window, undefined ) {
   $.extend( true, window, {
@@ -998,25 +998,27 @@
   var ColumnGroupHeader = iui.sheet.renderer.ColumnGroupHeader = function ( group ) {
     var renderer = $( "<li class='spreadsheet-column spreadsheet-column-group ui-state-default'><span>" + group.label + "<span></li>" )
       .button()
-      .removeClass( "ui-corner-all" )
-      .resizable( {
-        handles: "e",
-        minWidth: 2,
-        stop: function ( e, ui ) {
-          // var group = $(this).data("group");
-          // 그룹의 너비를 컬럼의 수로 나눈다.
-          var groupWidth = ui.size.width - ui.originalSize.width;
-          var columnWidth = Math.floor( groupWidth / group.columns.length );
-          var totalWidth = 0;
-          group.columns.each( function ( i ) {
-            var width = group.columns.length === i + 1 ? groupWidth - totalWidth : columnWidth;
-            this.width = this.ui.width() + width;
-            this.ui.width( this.width );
-            totalWidth += width;
+      .removeClass( "ui-corner-all" );
+        /*
+          .resizable( {
+            handles: "e",
+            minWidth: 2,
+            stop: function ( e, ui ) {
+              // var group = $(this).data("group");
+              // 그룹의 너비를 컬럼의 수로 나눈다.
+              var groupWidth = ui.size.width - ui.originalSize.width;
+              var columnWidth = Math.floor( groupWidth / group.columns.length );
+              var totalWidth = 0;
+              group.columns.each( function ( i ) {
+                var width = group.columns.length === i + 1 ? groupWidth - totalWidth : columnWidth;
+                this.width = this.ui.width() + width;
+                this.ui.width( this.width );
+                totalWidth += width;
+              } );
+              group.parent._renderColumns( group.parent.boundary.firstColumn );
+            }
           } );
-          group.parent._renderColumns( group.parent.boundary.firstColumn );
-        }
-      } );
+          */
     return renderer;
   };
 
