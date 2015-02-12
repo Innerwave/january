0e716586a346f4dc3e1fb2e99464fb9e76d9639d
Hyeongjin Yong <gilseonappa@gmail.com>
2015-02-12 13:58:23 +0900

    work history : v0.2.550-SNAPSHOT

--- iui-scroller.js
+++ iui-scroller.js
@@ -1,4 +1,4 @@
-/*! Innerwave Spreadsheet - v0.2.548-SNAPSHOT - 2015-02-12
+/*! Innerwave Spreadsheet - v0.2.549-SNAPSHOT - 2015-02-12
 * Copyright (c) 2015 innerwave.co.kr; Licensed  */
 ( function ( factory ) {
   if ( typeof define === 'function' && define.amd ) {
