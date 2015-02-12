b081f108a0882c84da69abb8296eef3f34d3f849
Hyeongjin Yong <gilseonappa@gmail.com>
2015-02-12 16:10:02 +0900

    work history : v0.2.600-SNAPSHOT

--- src/js/renderer/columnGroupHeader.js
+++ src/js/renderer/columnGroupHeader.js
@@ -9,29 +9,8 @@
 
 
   var ColumnGroupHeader = iui.sheet.renderer.ColumnGroupHeader = function ( group ) {
-    var renderer = $( "<li class='spreadsheet-column spreadsheet-column-group ui-state-default'><span>" + group.label + "<span></li>" )
-      .button()
-      .removeClass( "ui-corner-all" );
-        /*
-          .resizable( {
-            handles: "e",
-            minWidth: 2,
-            stop: function ( e, ui ) {
-              // var group = $(this).data("group");
-              // 그룹의 너비를 컬럼의 수로 나눈다.
-              var groupWidth = ui.size.width - ui.originalSize.width;
-              var columnWidth = Math.floor( groupWidth / group.columns.length );
-              var totalWidth = 0;
-              group.columns.each( function ( i ) {
-                var width = group.columns.length === i + 1 ? groupWidth - totalWidth : columnWidth;
-                this.width = this.ui.width() + width;
-                this.ui.width( this.width );
-                totalWidth += width;
-              } );
-              group.parent._renderColumns( group.parent.boundary.firstColumn );
-            }
-          } );
-          */
+    var renderer = $( "<span>" ).text( group.label );
+
     return renderer;
   };
 
