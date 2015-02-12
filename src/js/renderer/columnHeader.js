82e1c6701ebd6b2534c93173cac068d7e2b00d47
Hyeongjin Yong <gilseonappa@gmail.com>
2015-02-12 11:03:32 +0900

    윈도우 개발환경

--- src/js/renderer/columnHeader.js
+++ src/js/renderer/columnHeader.js
@@ -7,11 +7,22 @@
     }
   } );
 
-  iui.sheet.renderer.ColumnHeader = function ( column ) {
+  // ------------------------------------
+  // Private Area by Closure
+  // ------------------------------------
 
+  var Renderer = iui.sheet.renderer.ColumnHeaderRenderer = function ( cell ) {
+    cell = cell || {};
+
+    if ( !( this instanceof Renderer ) ) {
+      return new Renderer( cell );
+    }
+
+    var div = $( '<div>' ).attr( 'title', cell.value );
+    var span = $( '<span>' ).text( cell.value ).appendTo( div );
+
+    return div;
   };
 
-  $.extend( iui.sheet.renderer.ColumnHeader.prototype, {
 
-  } );
 }( jQuery, window ) );
